b7b4986f1aafdd655f8612f5a22d360fc44e7ccb
Douglas Negri <douglasmnegri@gmail.com>
2023-11-18 10:59:35 -0300

    The program is now running 5 times as intended. We have made some changes. We could improve this alot! I think we could fragment some parts of the code into smaller functions. Line 46 is an example of what could be improved.

--- file.js
+++ file.js
@@ -1,4 +1,17 @@
-function getComputerChoice() {
+            
+
+    // Initialize scores
+            let playerScore = 0;   
+            let computerScore = 0;
+    // This function is responsible for the number of times the game is played, and it loops every part of the game.
+    //We could have also used console.log() five times instead using the loop.
+    function playGame() {
+
+                for(let i = 0; i < 5; i++) { 
+                    
+                
+    // This function is responsible for the computer choice.
+    function getComputerChoice() {
     let randomNumber = Math.floor(Math.random() * (4-1) + 1);
     if(randomNumber === 1) {
         return "rock";
@@ -11,25 +24,42 @@ function getComputerChoice() {
     }
 }
 
-function playGame(playerSelection, computerSelection) {
-    let promptChoice = prompt("Rock, Paper or Scissors?").toLowerCase();
-    playerSelection = promptChoice;
-    computerSelection = getComputerChoice();
-    
-    if(playerSelection === "rock" && computerSelection === "scissors") {
-        return "You Win! Rock beats Scissors";
-    }
-    else if(playerSelection === "paper" && computerSelection === "rock") {
-        return "You Win! Paper beats Rock";
-    }
-    else if(playerSelection === "scissors" && computerSelection === "paper") {
-        return "You Win! Scissors beats Paper";
-    }
-    else if(playerSelection === computerSelection) {
-        return "It's a Draw! Play again!"
-    }
+// playRound intent is to run a match between the computer and the user.
+        function playRound(playerSelection, computerSelection) {
+             const win = (playerSelection === "rock" && computerSelection === "scissors") ||
+             (playerSelection === "paper" && computerSelection === "rock") ||
+             (playerSelection === "scissors" && computerSelection === "paper");
+             if(playerSelection === computerSelection) {
+                return "It's a tie!";
+            }
+             if(win) {
+                playerScore++;
+                return "You win!";
+             }
 
-    else {
-        return `You Lose! ${computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1)} beats ${playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1)}`;
-    }
-}
+             else {
+                computerScore++;
+                return "You Lose!";
+             }
+         
+            } 
+//We could make a new function and store the values of playerSelection inside of it, just like we did with computerChoice();
+             let player = prompt("Rock, Paper or Scissors?");
+             const playerSelection = player.toLowerCase();
+             const computerSelection = getComputerChoice();
+             const playRoundResult = playRound(playerSelection, computerSelection);
+             console.log(`Computer input: ${computerSelection}`);
+             console.log(`Player input: ${playerSelection}`);
+             console.log(playRoundResult);    
+     }
+  }
+
+
+playGame();
+// Why does it need to divide it by 2? Is it because of the flawed loop?
+// We fixed the variable outside the scope. It was incrementing 2 instead of 1.
+console.log("Computer: " + computerScore);
+console.log("Player: " + playerScore);
+
+
+
